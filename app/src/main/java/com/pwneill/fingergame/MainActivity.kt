0156c2b93529844e92d7133322e4affd11ab6405
Pwneill <phelan.oneill@gmail.com>
2021-01-14 18:27:24 -0600

    first commit

---
+++ app/src/main/java/com/pwneill/fingergame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pwneill.fingergame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
