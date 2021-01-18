9ad100e16e899384f858a339875968f95a7923a4
Pwneill <phelan.oneill@gmail.com>
2021-01-17 22:16:04 -0600

    it's not the hotfix we need, but it's the hotfix we deserve

--- app/src/main/java/com/pwneill/fingergame/MainActivity.kt
+++ app/src/main/java/com/pwneill/fingergame/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pwneill.fingergame
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,48 +15,66 @@ class MainActivity : AppCompatActivity() {
     private lateinit var timerTextView: TextView
     private lateinit var thousandTextView: TextView
     private lateinit var tapButton: Button
-    private lateinit var timer: CountDownTimer
+    private lateinit var countdownTimer: CountDownTimer
     private val timeIncrement: Long = 1000
     private var timeMm: Long = 30000
     private var clicks: Long = 300
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("savedInstance", "narroooooooo")
+        super.onSaveInstanceState(outState)
+        outState.putLong("timeRemaining", timeMm)
+        outState.putLong("clicks", clicks)
+        countdownTimer.cancel();
+        val truthy = countdownTimer == null
+        Log.i("timer", truthy.toString())
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        if (savedInstanceState != null) {
-            timeMm = savedInstanceState.getLong("timeRemaining")
-            clicks = savedInstanceState.getLong("clicks")
-        } else {
-            timeMm = 30000
-            clicks = 300
-        }
-
+        Log.i("timer", "On Create Methods is Called");
+        Log.i("timer", "onCreateYonder")
         timerTextView = findViewById(R.id.textView)
         thousandTextView = findViewById(R.id.textView2)
         tapButton = findViewById(R.id.button)
 
-        thousandTextView.text = clicks.toString()
+        if (savedInstanceState != null) {
+            timeMm =  savedInstanceState.getLong("timeRemaining")
+            clicks = savedInstanceState.getLong("clicks")
 
-        timer()
+            Log.i("timer", "yes!")
 
-        tapButton.setOnClickListener { //Your code here
+            countdownTimer.start()
 
-            clicks -= 1
+        } else {
             thousandTextView.text = clicks.toString()
-
-            if (timeMm > 0 && clicks <= 0) {
-
-                Toast.makeText(applicationContext, "Game Over!", Toast.LENGTH_SHORT).show()
-
-                gameReset(true)
-            }
+            timer(timeMm, timeIncrement)
         }
-    }
 
-    private fun timer () {
-    timer = object: CountDownTimer(timeMm, timeIncrement) {
+            tapButton.setOnClickListener { //Your code here
+
+                clicks -= 1
+                thousandTextView.text = clicks.toString()
+
+                if (timeMm > 0 && clicks <= 0) {
+
+                    Toast.makeText(applicationContext, "Game Over!", Toast.LENGTH_SHORT).show()
+
+                    gameReset(true)
+                }
+            }
+
+        }
+
+    private fun timer(time: Long, increment: Long) {
+
+        Log.i("timer", "yonder")
+
+        countdownTimer = object : CountDownTimer(time, increment) {
             override fun onTick(millisUntilFinished: Long) {
                 val timerText: String = "seconds remaining: " + millisUntilFinished / 1000
                 timerTextView.text = timerText
@@ -65,9 +84,11 @@ class MainActivity : AppCompatActivity() {
                 timerTextView.text = "done!"
                 gameReset(false)
             }
-        }.start()
+        }
+        countdownTimer.start()
 
-}
+    }
+
     private fun gameReset(winner: Boolean) {
 
         val builder = AlertDialog.Builder(this@MainActivity)
@@ -84,19 +105,24 @@ class MainActivity : AppCompatActivity() {
             val clicks: Long = 300
             thousandTextView.text = clicks.toString()
 
-            val time = 30000
+            val time: Long = 30000
             timerTextView.text = time.toString()
 
-            timer()
+            timer(time, timeIncrement)
         }
         builder.setCancelable(false)
         builder.show()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putLong("timeRemaining", timeMm)
-        outState.putLong("clicks", clicks)
-        timer.cancel()
+    override fun onResume() {
+        super.onResume()
+
+        val newTimer = timerTextView.text.split(": ")
+        if (newTimer.size >= 2) {
+            timeMm = newTimer[1].toLong() * 1000
+            timer (timeMm, timeIncrement)
+        }
     }
+
+
 }
